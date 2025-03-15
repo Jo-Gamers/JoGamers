e493e124a9d7f09884ce8502fd33e290aa5d7b37
A7madtabaza <ahmadtabaza272@gmail.com>
2025-03-15 20:22:17 +0300

    login and register page

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const auth = require("../controllers/userController");
 
 const router = express.Router();
@@ -9,17 +8,3 @@ router.post("/login", auth.login);
 
 
 module.exports = router;
-=======
-const { getProfile, updateProfile } = require("../controllers/userController");
-const authMiddleware = require("../middlewares/authMiddleware");
-
-const router = express.Router();
-
-// جلب بيانات المستخدم (يتطلب مصادقة)
-router.get("/profile", authMiddleware, getProfile);
-
-// تحديث بيانات المستخدم (يتطلب مصادقة)
-router.put("/profile", authMiddleware, updateProfile);
-
-module.exports = router;
->>>>>>> bf672c1e409d1645fff773f7adb6b92546d1f13c
